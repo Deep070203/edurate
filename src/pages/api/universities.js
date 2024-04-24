beba172656d6493ad1fc0ecaaaf7e44f94156ca1
Deep Shah <66430880+Deep070203@users.noreply.github.com>
2024-04-24 17:30:14 -0400

    minor changes

--- src/pages/api/universities.js
+++ src/pages/api/universities.js
@@ -1,4 +1,4 @@
-import dbConnect from '../../utils/dbConnect';
+import { dbConnect } from '../../../utils/dbConnect.js';
 import University from '../../models/university';
 
 export default async function handler(req, res) {
