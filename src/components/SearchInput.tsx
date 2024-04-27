8fc0c63ceeedcc6e8aa0f24a7c5f31e80f82410c
Aryaman Patel <132040773+1aryamanp@users.noreply.github.com>
2024-04-27 00:06:54 -0400

    Removed Login Page, Upgraded Homepage

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -48,10 +48,7 @@ export const SearchInput = ({ defaultValue, placeholder }: iDefault) => {
     };
 
     return (
-        <div className="search__input relative border-[3px] border-solid border-slate-700 flex items-center gap-4 p-4 rounded-[20px]" style={{ minWidth: '500px' }}>
-            <div style={{ flexShrink: 0 }}>
-                <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
-            </div>
+        <div className="relative border-[3px] border-solid border-slate-400 flex items-center gap-4 p-4 rounded-[10px]" style={{ minWidth: '700px', backgroundColor: '#e5e5e5' }}>
             <input
                 type="text"
                 id="inputId"
@@ -59,13 +56,16 @@ export const SearchInput = ({ defaultValue, placeholder }: iDefault) => {
                 value={inputValue}
                 onChange={handleChange}
                 onKeyDown={handleKeyPress}
-                className="bg-gray-800 rounded-[15px] outline-none border-none w-full py-2 pl-10 pr-3 text-lg"
-                style={{ flexGrow: 1 }}
+                className="rounded-[10px] outline-none border-none w-full py-2 pl-10 pr-3 text-lg text-black"
+                //style={{ flexGrow: 1, backgroundColor: '#939393' }}
             />
+            <div style={{ flexShrink: 0 }}>
+                <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
+            </div>
             {showSuggestions && (
-                <ul className="absolute z-10 w-100% bg-gray-600 shadow-md overflow-auto rounded-[12px]" style={{ top: '95%', left: '11%' }}>
-                    {suggestions?.map((suggestion, index) => (
-                        <li key={index} onClick={() => handleSearch(suggestion)} className="p-2 hover:bg-gray-800 cursor-pointer">
+                <ul className="absolute z-10 w-full bg-gray-600 shadow-md overflow-auto rounded-[12px]" style={{ top: '100%', left: '0%' }}>
+                    {suggestions.map((suggestion, index) => (
+                        <li key={index} onClick={() => handleSearch(suggestion)} className="p-2 hover:bg-gray-800 cursor-pointer text-black">
                             {suggestion}
                         </li>
                     ))}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -12,33 +12,83 @@ const Home = () => {
     }, []);
 
     const totalUser = courseData.length;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        //event.preventDefault();
+        // Add your login logic here
+        // Reset email and password after submitting if necessary
+    };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-dark">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#e5e5e5]">
             <Head>
                 <title>Edurate</title>
             </Head>
 
             {/* Header Section */}
-            <header className="w-full bg-gray-800 py-4 px-8 flex justify-between items-center">
+            <header className="w-full bg-[#14213d] py-4 px-8 flex justify-between items-center">
                 <h1 className="text-xl font-bold text-white">EduRate</h1>
                 <div className="flex-grow flex justify-center">
                     <SearchInput placeholder="What university do you go to?" defaultValue={""} />
                 </div>
-                <div className="flex">
-                    <Link href="../Login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</Link>
-                    <Link href="../Signup" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-4 rounded">Sign Up</Link>
-                </div>
             </header>
 
             {/* Main Content */}
-            <div className="flex-grow flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold mb-4 text-white">Welcome to Edurate!</h1>
-                <p className="text-lg text-gray-400 mb-6">Here you will get honest student reviews of all the courses.</p>
+            <div className="flex flex-grow w-full">
+                <div className="flex-[6.5] flex items-center justify-center overflow-x-auto">
+                    <div className="min-w-[850px]">
+                        <h2 className="text-center text-xl font-bold text-black mb-4">Popular Universities</h2>
+                        <div className="grid grid-cols-5 gap-4 p-4">
+                            {Array.from({ length: 10 }).map((_, index) => (
+                                <div key={index} className="w-50 h-40 bg-[#14213d] rounded-lg shadow-lg"></div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                {/* Login Form */}
+                <div className="flex-[3.5] flex items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <div className="block text-black text-sm font-bold mb-2 justify-center">
+                            <p className="text-center text-xl font-bold text-black mb-4">Here you will get honest student reviews of all the courses.</p>
+                        </div>
+                        <form onSubmit={handleLogin} className="bg-[#939393] shadow-md rounded px-8 pt-6 pb-8">
+                            <div className="mb-4">
+                                <label className="block text-black text-sm font-bold mb-2" htmlFor="email">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-6">
+                                <label className="block text-black text-sm font-bold mb-2" htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <Link href="" className="bg-[#fca311] hover:bg-[#fca311]/90 text-black font-bold py-2 px-4 rounded">Login</Link>
+                            <Link href="../Signup" className="bg-[#fca311] hover:bg-[#fca311]/90 text-black font-bold py-2 px-4 ml-4 rounded">Sign Up</Link>
+                        </form>
+                    </div>
+                </div>
             </div>
 
             {/* Footer Section */}
-            <footer className="w-full bg-gray-800 py-4 px-8 text-center text-white">
+            <footer className="w-full bg-[#14213d] py-4 px-8 text-center text-white">
                 <p> 2024 Edurate. All rights reserved. </p>
             </footer>
             
