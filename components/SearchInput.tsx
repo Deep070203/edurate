37230b9824a39117384df10380a092e1b173e9de
Aryaman Patel <132040773+1aryamanp@users.noreply.github.com>
2024-04-29 15:55:23 -0400

    fixed all errors and directory issues

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent, useEffect } from "react";
-import searchIcon from '../public/images/search';
+import searchIcon from '../public/images/search.png';
 import Image from 'next/image';
 
 interface iDefault {
